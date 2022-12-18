5bfaf58cf17fba22cf1c880b6d3585b66d2a8b30
Hoi Wong <jameshf420@gmail.com>
2022-12-17 19:06:34 -0500

    added div- user-value to for js to select element

--- html/Script/script.js
+++ html/Script/script.js
@@ -3,10 +3,10 @@ const formEl = document.querySelector('form');
 // add eventlisteer to the from (when user sumbit it )
 formEl.addEventListener('submit', function(event){
     event.preventDefault();
-    
+
     const userComment = document.querySelector('#yourComment').value;
     /* const userName = document.querySelector('#displayName').value; */
-    
+
     //genrate a date for the comment 
     // collect the data (users comment)
     const postedComment = document.querySelector('.test');
@@ -14,4 +14,4 @@ formEl.addEventListener('submit', function(event){
     postedComment.innerText = userComment;
     formEl.reset()
 });
-// 3 to do , user name into pics alt text , genarate the date , appen the comment 
+// 3 to do , user name into pics alt text , genarate the date , appen the comment
